f1e67e6332c638f0745ab963f1f5c0ae1c88bade
WhyK <windchimeyk@gmail.com>
2024-04-05 11:09:59 +0900

    feat(quickstart): setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
+const Button = () => {
+  return <button type="button">I'm a button</button>;
+};
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Welcome to my app</h1>
+      <Button />
+    </div>
   );
-}
-
-export default App;
+};
